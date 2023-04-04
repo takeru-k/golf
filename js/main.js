23d4bb5abb62bbf2181c0ab41e96f9e3ac6e2168
takeru-k <kam41893@gmail.com>
2023-04-04 09:37:08 +0900

    index2を追加

--- js/main.js
+++ js/main.js
@@ -62,3 +62,26 @@ $(function () {
     startProgressbar();
   });
 });
+
+//pagetop
+$(function() {
+var topBtn = $('.pagetop');    
+topBtn.hide();
+//スクロールが100に達したらボタン表示
+$(window).scroll(function () {
+if ($(this).scrollTop() > 100) {
+//ボタンの表示方法
+topBtn.fadeIn();
+} else {
+//ボタンの非表示方法
+topBtn.fadeOut();
+}
+});
+//スクロールしてトップ
+topBtn.click(function () {
+$('body,  html').animate({
+scrollTop: 0
+},   500);
+return false;
+});
+});
